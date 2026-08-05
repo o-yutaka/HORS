2f273bf465faf4307004ca190d1e63418db1b088
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-08-05 13:05:54 +0900

    feat: add competition operator public API

---
+++ capability/competition_operator/__init__.py
@@ -0,0 +1,24 @@
+from .contracts import (
+    ArenaVerdict,
+    CandidateSkill,
+    CompetitionRun,
+    Evidence,
+    FrozenSubmission,
+    Hypothesis,
+    RunStage,
+    TrialResult,
+)
+from .operator import CompetitionOperator, OperatorPorts
+
+__all__ = [
+    "ArenaVerdict",
+    "CandidateSkill",
+    "CompetitionOperator",
+    "CompetitionRun",
+    "Evidence",
+    "FrozenSubmission",
+    "Hypothesis",
+    "OperatorPorts",
+    "RunStage",
+    "TrialResult",
+]
